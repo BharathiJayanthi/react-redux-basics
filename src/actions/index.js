29e5293f794cf1f5e978a241870a065c12d39caa
bharathijayanthi5 <bharathi@gurujada.org>
2018-06-20 12:38:48 +0530

    created a new react-redux app and learned how to use redux with react

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+
+
+export function moviesList(){
+
+	return {
+		type:'MOVIES_LIST',
+		payload:[
+            {id:1,name:"Pulp Fiction"},
+            {id:2,name:"Pulp Adventure"},
+            {id:3,name:"Pulp Comedy"}
+
+		]
+	}
+}
